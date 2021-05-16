c0ab582fc9602df659d1eea139a124b3ce35470b
Tilzen <lucas.nascimentoR@hotmail.com>
2021-05-16 02:12:37 -0300

    feat: Implementação de contract de memes.

--- src/contracts/Meme.sol
+++ src/contracts/Meme.sol
@@ -3,6 +3,18 @@ pragma solidity ^0.5.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
 
 contract Meme is ERC721Full {
+    string[] public memes;
+    mapping(string => bool) _memeExists;
+
     constructor() ERC721Full("Meme", "MEME") public {
+    }
+
+    function mint(string memory _meme) public {
+        require(!_memeExists[_meme]);
+
+        uint _id = memes.push(_meme);
+
+        _mint(msg.sender, _id);
+        _memeExists[_meme] = true;
     }
 }
